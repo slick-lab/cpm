2aedf04942981c3b4d960908192c2a0b730a68fb
ZenDrx <ynwghosted@icloud.com>
2026-06-19 10:39:41 +0100

    Create init.cr

---
+++ src/cpm/init.cr
@@ -0,0 +1,20 @@
+module Cpm
+  class Init
+    def init
+     process = Process.run(
+        "shards",
+        args: [init],
+        output: Process::Redirect::Inherit
+        error: Process::Redirect::Inherit
+      )
+      if process.exit_code == 0
+       puts "Done."
+     else
+      puts "failed with #{process.exit_code}"
+      exit 1 
+     end
+   end
+ end
+end
+    
+        
