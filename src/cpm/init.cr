7b41cb9474e2cb492e95fd985c5f8c85e9305dbf
ZenDrx <ynwghosted@icloud.com>
2026-06-24 13:43:07 +0100

    Update init.cr

--- src/cpm/init.cr
+++ src/cpm/init.cr
@@ -1,7 +1,26 @@
-
+require "path"
+module Cli
   class Init
-    def init
-     process = Process.run(
+    def run(args : Array(String))
+      i = 0
+      while i < args.size
+      case args[i]
+      when "--cache"
+       turn_cache
+      else
+       init_project
+     end
+   end 
+   def turn_cache
+     cache_dir = Path.home / ".cpm"
+     Dir.mkdir_p(cache_dir)
+     File.write(cache_dir / "cache.txt", "")
+     puts "created cache.."
+     init_project
+  end
+
+  def init_project
+    process = Process.run(
         "shards",
         args: [init],
         output: Process::Redirect::Inherit
@@ -15,3 +34,4 @@
      end
    end
  end
+end
