fbcaf5338c5369880d48de5f2692f809d9b0d00b
ZenDrx <ynwghosted@icloud.com>
2026-06-24 13:47:08 +0100

    Update init.cr

--- src/cpm/init.cr
+++ src/cpm/init.cr
@@ -1,37 +1,36 @@
 require "path"
+
 module Cli
   class Init
     def run(args : Array(String))
-      i = 0
-      while i < args.size
-      case args[i]
-      when "--cache"
-       turn_cache
+      if args.includes?("--cache")
+        turn_cache
       else
-       init_project
-     end
-   end 
-   def turn_cache
-     cache_dir = Path.home / ".cpm"
-     Dir.mkdir_p(cache_dir)
-     File.write(cache_dir / "cache.txt", "")
-     puts "created cache.."
-     init_project
-  end
+        init_project
+      end
+    end
 
-  def init_project
-    process = Process.run(
+    def turn_cache
+      cache_dir = Path.home / ".cpm"
+      Dir.mkdir_p(cache_dir)
+      File.write(cache_dir / "cache.txt", "")
+      puts "Created cache."
+      init_project
+    end
+
+    def init_project
+      process = Process.run(
         "shards",
-        args: [init],
-        output: Process::Redirect::Inherit
+        args: ["init"],
+        output: Process::Redirect::Inherit,
         error: Process::Redirect::Inherit
       )
       if process.exit_code == 0
-       puts "Done."
-     else
-      puts "failed with #{process.exit_code}"
-      exit 1 
-     end
-   end
- end
+        puts "Done."
+      else
+        puts "Failed with #{process.exit_code}"
+        exit 1
+      end
+    end
+  end
 end
